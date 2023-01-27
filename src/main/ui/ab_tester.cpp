5693602afed088ff520dfcfb36a7a555e440d7af
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-01-27 20:33:06 +0300

    Initial import from plugin template

---
+++ src/main/ui/ab_tester.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-plugins-ab-tester
+ * Created on: 25 нояб. 2020 г.
+ *
+ * lsp-plugins-ab-tester is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-plugins-ab-tester is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-plugins-ab-tester. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <lsp-plug.in/plug-fw/ui.h>
+#include <private/plugins/ab_tester.h>
+
+namespace lsp
+{
+    namespace plugui
+    {
+        //---------------------------------------------------------------------
+        // Plugin UI factory
+        static const meta::plugin_t *plugin_uis[] =
+        {
+            &meta::ab_tester_mono,
+            &meta::ab_tester_stereo
+        };
+
+        static ui::Factory factory(plugin_uis, 2);
+
+    } /* namespace plugui */
+} /* namespace lsp */
+
+
